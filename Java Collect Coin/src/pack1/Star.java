4bbe768225ad313717c9054033243329c2ef4f76
muin-unity-comp111 <53714737+muin-unity-comp111@users.noreply.github.com>
2019-11-23 11:44:05 +1100

    Java Collect Coins -2
    Saved version of Star

--- Java Collect Coin/src/pack1/Star.java
+++ Java Collect Coin/src/pack1/Star.java
@@ -1,5 +1,55 @@
 package pack1;
 
-public class Star {
+import java.awt.Color;
+import java.awt.Graphics;
 
+public class Star {
+	private int xPos;
+	private int yPos;
+	
+	private int width;
+	private int height;
+	
+	private int red;
+	private int green;
+	private int blue;
+	
+	public Star()
+	{
+		super();
+	}
+	
+	public Star(int xPos, int yPos, int width, int height)
+	{
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public void randomizeColour()
+	{
+		// Will be defined
+	}
+	
+	public Color randomizeColor()
+	{
+		// Will be defined
+		return Color.black;
+		
+	}
+	// 0-255
+	public int randomizeColor(int limit)
+	{
+		// Will be defined
+		return 0;
+	}
+	
+	public void drawStar(Graphics g)
+	{
+		
+	}
+	
+	
 }
+
